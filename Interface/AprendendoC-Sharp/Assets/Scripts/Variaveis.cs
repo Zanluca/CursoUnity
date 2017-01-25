66f0e9bf95ef9cc2e04e749a9ae637a50c595fda
Gabriel Zanluca <gabriel.zanluca@gmail.com>
2017-01-25 20:45:50 -0200

    Variáveis: O que são e como definir

---
+++ Interface/AprendendoC-Sharp/Assets/Scripts/Variaveis.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variaveis : MonoBehaviour {
+
+	// tipos básicos: inteiro = int, ponto flutuante = float, texto = string, booleano = bool 
+	// tipo de acesso (opcional) + tipo da variável  (obrigtório) + nome da variável (obrigatório) + = valor (opcional)
+	public int meuInteiro;
+	public float meuFlutuandte;
+	public string meuTexto;
+	public bool meuBooleano;
+	public int velocidadeDoMeuPersonagem;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
