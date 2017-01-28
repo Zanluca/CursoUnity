f25fece39922f3509ed3729ad8a3d2b8e3fdfa10
Gabriel Zanluca <gabriel.zanluca@gmail.com>
2017-01-28 20:09:49 -0200

    Comandos Condicionais I: Entendendo a Estrutura

---
+++ Interface/AprendendoC-Sharp/Assets/Scripts/ComandosCondicionais.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComandosCondicionais : MonoBehaviour {
+
+	public bool condicao; // pode ser true ou false
+	public int idade;
+
+	// Use this for initialization
+	void Start () {
+		if (condicao) {
+			// tudo que estiver enre esssa chaves vai ser executado quando condicao for verdadeira
+			idade = 0;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
