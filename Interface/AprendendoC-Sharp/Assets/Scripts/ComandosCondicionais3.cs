61fc2562fc0a00f1a34935f5c1b97db0fb45078e
Gabriel Zanluca <gabriel.zanluca@gmail.com>
2017-02-12 21:45:35 -0200

    #07 - Comandos Condicionais III: Operações Lógicas

---
+++ Interface/AprendendoC-Sharp/Assets/Scripts/ComandosCondicionais3.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComandosCondicionais3 : MonoBehaviour {
+
+	public int idade;
+	public bool idValida;
+	public bool compraAutorizada;
+
+	// Use this for initialization
+	void Start () {
+		if ((idade >= 19 && idValida) || (idade >= 40 )) {
+			compraAutorizada = true;
+		} 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
