b7b88853ff23ce47d421836d772eb1e99ef8c422
Gabriel Zanluca <gabriel.zanluca@gmail.com>
2017-02-15 21:29:46 -0200

    #09 - Arrays

---
+++ Interface/AprendendoC-Sharp/Assets/Scripts/Arrays.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrays : MonoBehaviour {
+
+	public int[] arrayDeInteiros;
+	public int valorQualquer;
+
+	// Use this for initialization
+	void Start () {
+		arrayDeInteiros [0] = -3;
+		arrayDeInteiros [3] = 7;
+		valorQualquer = arrayDeInteiros [5];
+		int posicaoDoVetor = 0;
+		while (posicaoDoVetor < arrayDeInteiros.Length) {
+			arrayDeInteiros [posicaoDoVetor] = posicaoDoVetor * 2;
+			posicaoDoVetor++;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
