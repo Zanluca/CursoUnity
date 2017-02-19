1a1fffdcf3d3bcbee325e257ed56113d8dc67eb8
Gabriel Zanluca <gabriel.zanluca@gmail.com>
2017-02-19 12:03:08 -0300

    #10 - Loops II: For e Foreach

---
+++ Interface/AprendendoC-Sharp/Assets/Scripts/Loops2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops2 : MonoBehaviour {
+
+	public string[] textos;
+
+	// Use this for initialization
+	void Start () {
+		/*for (int varLocal = 1; varLocal <= 10; varLocal++) {
+			int multiploAtualDe3 = varLocal * 3;
+			print (multiploAtualDe3);
+		}*/
+
+//		for (int indice = 0; indice < textos.Length; indice++) {
+//			print (textos [indice]);
+//		}
+
+		//forma compacta de iterar (passar por) todos os elemntos de um array
+		foreach (string s in textos) {
+			print (s);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
