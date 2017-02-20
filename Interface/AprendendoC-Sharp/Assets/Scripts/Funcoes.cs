38d70ac01ed1659d7097f540b441873807a20016
Gabriel Zanluca <gabriel.zanluca@gmail.com>
2017-02-19 21:26:02 -0300

    #11 - Funções II: Como definir funções em C#

---
+++ Interface/AprendendoC-Sharp/Assets/Scripts/Funcoes.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Funcoes : MonoBehaviour {
+
+	//tipo de retorno (matéria prima) nome da funćão (argumentos (maetrias brutas))
+	//{
+	//}
+	public int latidos;
+	public int valorA, valorB;
+	public float resultado;
+
+	// Use this for initialization
+	void Start () {
+		//SomarEImprimir (valorA, valorB);
+		resultado = Multiplicar(valorA, valorB);
+	}
+
+	void Latir(){
+		print ("Whoof!");
+		latidos++;
+	}
+
+	void SomarEImprimir(float a, float b){
+		float soma = a + b;
+		print ("O resultado da soma e': "+soma);
+	}
+
+	float Multiplicar(float valor1, float valor2){
+		float multiplicacao = valor1 * valor2;
+		return multiplicacao;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Latir ();
+	}
+}
